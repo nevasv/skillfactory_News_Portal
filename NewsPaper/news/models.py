072ff785f35bbb0af9b6919360ba9c32cc22f41e
Vladmspb <nevasv@yandex.ru>
2023-01-25 18:01:02 +0300

    Поля базы из вебинара

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,78 +1,115 @@
 from django.db import models
+from django.contrib.auth.models import User
+from param import *
+from django.db.models import Sum
 
 
 # Модель Author
 # Имеет следующие поля:
-# -cвязь «один к одному» с встроенной моделью пользователей User;
-# -рейтинг пользователя.
+# -<portalUser> cвязь «один к одному» с встроенной моделью пользователей User;
+# - <ratingAuthor> рейтинг пользователя.
 class Author(models.Model):
+    authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
+    ratingAuthor = models.SmallIntegerField(default=0)
 
-    # Метод update_rating() модели Author, который обновляет рейтинг пользователя, переданный в аргумент этого метода.
+    # Методы
+
+    # - update_rating() модели Author, который обновляет рейтинг пользователя, переданный в аргумент этого метода.
     # Он состоит из следующего:
-    # -суммарный рейтинг каждой статьи автора умножается на 3;
-    # -суммарный рейтинг всех комментариев автора;
-    # -суммарный рейтинг всех комментариев к статьям автора.
+    #    -суммарный рейтинг каждой статьи автора умножается на 3;
+    #    -суммарный рейтинг всех комментариев автора;
+    #    -суммарный рейтинг всех комментариев к статьям автора.
     def update_rating(self):
-        pass
+        postRat = self.post_set.aggrigate(postRating=Sum('rating'))
+        pRat = 0
+        pRat += postRat.get('postRating')
+
+        commenttRat = self.authorUser.comment_set.aggrigate(postRating=Sum('rating'))
+        cRat = 0
+        cRat += postRat.get('commentRating')
+
+        self.ratingAuthor = pRat * 3 + cRat
+        self.save()
 
 
 # Модель Category
 # Категории новостей/статей — темы, которые они отражают (спорт, политика, образование и т. д.).
 # Имеет единственное поле: название категории.
-# Поле должно быть уникальным (в определении поля необходимо написать параметр unique = True).
+#  - <name> Поле должно быть уникальным (в определении поля необходимо написать параметр unique = True).
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=64, unique=True)
 
 
 # Модель Post
-# Эта модель должна содержать в себе статьи и новости, которые создают пользователи. Каждый объект может иметь одну или несколько категорий.
+# Эта модель должна содержать в себе статьи и новости, которые создают пользователи.
+# Каждый объект может иметь одну или несколько категорий.
 # Соответственно, модель должна включать следующие поля:
-# -связь «один ко многим» с моделью Author;
-# -поле с выбором — «статья» или «новость»;
-# -автоматически добавляемая дата и время создания;
-# -связь «многие ко многим» с моделью Category (с дополнительной моделью PostCategory);
-# -заголовок статьи/новости;
-# -текст статьи/новости;
-# -рейтинг статьи/новости.
+#   - <author>        связь «один ко многим» с моделью Author;
+#   - <categoryFild>  поле с выбором — «статья» или «новость»;
+#   - <dataCreations> автоматически добавляемая дата и время создания;
+#   - <postCategory>  связь «многие ко многим» с моделью Category (с дополнительной моделью PostCategory);
+#   - <title>         заголовок статьи/новости;
+#   - <text>          текст статьи/новости;
+#   - <rating>        рейтинг статьи/новости.
 class Post(models.Model):
-    pass
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categoryFild = models.CharField(max_length=2, choices=CATEGORY_CHOISES, default=ARTICLE)
+    dataCreations = models.DateTimeField(auto_now_add=True)
+    postCategory = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
 
-    # Метод preview() модели Post, который возвращает начало статьи (предварительный просмотр) длиной 124 символа и добавляет многоточие в конце.
-
-    # - like() ,
-    # - dislike(),
+    # Методы
+    # - preview() модели Post, который возвращает начало статьи
+    #             (предварительный просмотр) длиной 124 символа и добавляет многоточие в конце.
+    # - like()    которыq увеличивает рейтинг на единицу.
+    # - dislike() который уменьшают рейтинг на единицу.
     def like(self):
-        pass
+        self.rating += 1
+        self.save()
 
     def dislike(self):
-        pass
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[0:128] + '...'
 
 
 # Модель PostCategory
 # Промежуточная модель для связи «многие ко многим»:
-#
-# связь «один ко многим» с моделью Post;
-# связь «один ко многим» с моделью Category.
+# - <postThrough>     связь «один ко многим» с моделью Post;
+# - <categoryThrough> связь «один ко многим» с моделью Category.
 class PostCategory(models.Model):
-    pass
+    postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
+    categoryThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 # Модель Comment
 # Под каждой новостью/статьёй можно оставлять комментарии, поэтому необходимо организовать их способ хранения тоже.
 # Модель будет иметь следующие поля:
-#
-# связь «один ко многим» с моделью Post;
-# связь «один ко многим» со встроенной моделью User (комментарии может оставить любой пользователь, необязательно автор);
-# текст комментария;
-# дата и время создания комментария;
-# рейтинг комментария.
-class Comment(models.Model):
-    pass
+# - <commentPost>  связь «один ко многим» с моделью Post;
+# - <userPost>     связь «один ко многим» со встроенной моделью User
+#                  (комментарии может оставить любой пользователь, необязательно автор);
+# - <text>         текст комментария;
+# - <dataCreation> дата и время создания комментария;
+# - <rating>       рейтинг комментария.
 
-    # - like() ,
-    # - dislike(),
+class Comment(models.Model):
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    userPost = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    dataCreation = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
+
+    # Методы
+    # - like()    которыq увеличивает рейтинг на единицу.
+    # - dislike() который уменьшают рейтинг на единицу.
     def like(self):
-        pass
+        self.rating += 1
+        self.save()
 
     def dislike(self):
-        pass
+        self.rating -= 1
+        self.save()
