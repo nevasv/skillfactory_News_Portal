45fe9222a43eb7cf3cf6c9686ddbfcfb1c06c96b
vladmspb <nevasv@yandex.ru>
2023-03-11 17:32:59 +0300

    Последний пост на сайте

--- NewsPaper/news/templatetags/news_tags.py
+++ NewsPaper/news/templatetags/news_tags.py
@@ -6,13 +6,16 @@ register = template.Library()
 
 @register.simple_tag()
 def get_posts():  # get_posts(name='gp')
-
     return Post.objects.all().order_by('-rating')[:2]
+
+
+@register.simple_tag()
+def get_latest_post():
+    return Post.objects.all().latest('dataCreations')
 
 
 @register.inclusion_tag('news/tag.html')
 def show_comments():
-
     comments3 = Comment.objects.all().order_by('-rating')[:2]
 
     return {'comments3': comments3}
